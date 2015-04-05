7436adcad687bbdc620796961b05544e424a4c6a
Vinícius Deláscio <vinidroid@gmail.com>
2015-04-04 22:39:54 -0300

    Adding more stuff

--- NodeJs/create-emitter-and-listen-to-message.js
+++ NodeJs/create-emitter-and-listen-to-message.js
@@ -1,7 +1,17 @@
 var events = require('events');
 var EventEmitter = events.EventEmitter;
-var chat = new EventEmitter();
 
-chat.on('message', function(message){
-		console.log(message);
+var chat = new EventEmitter();
+var users = [], chatlog = [];
+
+chat.on('message', function(message) {
+  chatlog.push(message);
 });
+
+chat.on('join', function(nickname) {
+  users.push(nickname);
+});
+
+// Emit events here
+chat.emit('join', 'Hello');
+chat.emit('message', 'Hi guys this the message event');
