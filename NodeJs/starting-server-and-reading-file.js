c5364d13791acaa4027b550eb33bb7435cd4dcbe
Vinícius Deláscio <vinidroid@gmail.com>
2015-04-04 22:19:05 -0300

    Adding a Content-Type
    Adding a content-type on the response.writeHead.

--- NodeJs/starting-server-and-reading-file.js
+++ NodeJs/starting-server-and-reading-file.js
@@ -2,7 +2,8 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(request, response) {
-  response.writeHead(200);
+  response.writeHead(200, {
+  'Content-Type': 'text/html'});
   fs.readFile("index.html", function(err, content){
   	response.write(content);
     response.end();
