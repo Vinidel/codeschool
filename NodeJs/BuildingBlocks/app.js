9d5ce5615be7ad6e5ffc5512e2245a832a63a5fc
Vinícius Deláscio <vinidroid@gmail.com>
2015-04-08 22:40:38 -0300

    making changes on the app

--- NodeJs/BuildingBlocks/app.js
+++ NodeJs/BuildingBlocks/app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 
-app.get('/', function (request, response) {
-  response.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static('public'));
+
+// app.get('/', function (request, response) {
+//   response.sendFile(__dirname + '/public/index.html');
+// });
 
 app.listen(3000, function(){
 	console.log('Running Express');
