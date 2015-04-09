0804aa82482f58da9652cb02623e0cd7c871bd9d
Vinícius Deláscio <vinidroid@gmail.com>
2015-04-08 23:24:56 -0300

    Adding coments

--- NodeJs/BuildingBlocks/app.js
+++ NodeJs/BuildingBlocks/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+//this uses the logger middleware in all requests of the page
+var logger = require('./logger');
+app.use(logger);
 
 app.use(express.static('public'));
 
