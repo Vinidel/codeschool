015950a504d98f375a6a0a6a7780d6c3f39cd817
Vinícius Deláscio <vinidroid@gmail.com>
2015-04-09 22:14:26 -0300

    Adding Dynamic Route
    Adding a dynamic route that uses the request.params

--- NodeJs/BuildingBlocks/app.js
+++ NodeJs/BuildingBlocks/app.js
@@ -7,10 +7,36 @@ app.use(logger);
 
 app.use(express.static('public'));
 
+var dynamicBlocks = {
+  'Fixed':'Fastened securely in position',
+  'Movable': 'Capable of being moved',
+  'Rotating': 'Moving in a circle around its center'
+};
+
+
+//working with params on the request
 app.get('/blocks', function (request, response) {
   var blocks = ['Fixed','Movable','Rotating'];
-  response.json(blocks);
+  if(request.query.limit >= 0){
+    response.json(blocks.slice(0, request.query.limit));
+  }else {
+    response.json(blocks);
+  }
 });
+
+//adding a dynamic route
+app.get('/dynamic-blocks/:name', function(request, response){
+  var description = dynamicBlocks[request.params.name];
+  if(!description){
+    response.status(404).json('No description found for ' + request.params.name);
+  } else {
+    response.json(description);
+  }
+});
+
+
+
+
 
 app.listen(3000, function(){
 	console.log('Running Express');
